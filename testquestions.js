296da5f44f4d78551ec9ef5295b4285b0086236b
029614 <46335776+029614@users.noreply.github.com>
2019-06-02 05:38:59 -0700

    Add files via upload

---
+++ testquestions.js
@@ -0,0 +1,13 @@
+let question0 = ['question', 'answer 1', 'answer 2', 'answer 3', 'answer 4', 'answer 5', 'Correct Answer'];
+
+let question1 = ['I am an odd number. Take away a letter and I become even. What number am I?', 'One', 'Eight', 'Nineteen', 'Fifty', 'Seven', 'Seven'];
+
+let question2 = ['What has hands but doesn’t clap?', 'A Seal', 'A Tiger', 'A Clock', 'A Chimpanzee', 'A Cup', 'A Clock'];
+
+let question3 = ['What has to be broken before you can use it?', 'An Egg', 'A Vase', 'An Airplane', 'A Violin', 'A Table', 'An Egg'];
+
+let question4 = ['If two’s company, and three’s a crowd, what are four and five?', 'Fourty-Five', 'Twenty', 'Nine', 'ToolBox', 'Three', 'Nine'];
+
+let question5 = ['What five-letter word becomes shorter when you add two letters to it?', 'Tread', 'Spite', 'Short', 'Shave', 'Light', 'Short'];
+
+let questions = [question1, question2, question3, question4, question5];
